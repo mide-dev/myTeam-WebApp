ae9e5debd8104332b9ef472be34c5706a81ce1ce
midedev <ibrahimayomide330@gmail.com>
2022-10-03 11:31:52 +0100

    build Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import styles from "../styles";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div
+      className={`${styles.flexDefault} mt-[2rem] sm:mt-[3rem] 
+        pb-[12.313rem]`}
+    >
+      <div className="sm:mr-4">
+        <h1
+          className={`${styles.heroText} text-center
+           sm:text-left `}
+        >
+          Find the <br /> best <span className="text-lightCoral">talent</span>
+        </h1>
+      </div>
+
+      <div className="flex">
+        <div className="blue-border hidden sm:block" />
+        <p
+          className={`${styles.paragraph1} text-center 
+          sm:text-left mx-auto sm:self-end pt-[1.125rem] ss:pt-[1.5rem] `}
+        >
+          Finding the right people and building high performing teams can be
+          hard. Most companies aren’t tapping into the abundance of global
+          talent. We’re about to change that.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
