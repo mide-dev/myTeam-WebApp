146a75e48b4aa445c42dbec7d285bc77d4169353
midedev <ibrahimayomide330@gmail.com>
2022-10-03 22:56:51 +0100

    update assets

---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import hamburger from "./hamburger.svg";
+import open from "./open.svg";
+import close from "./close.svg";
+import person from "./person.svg";
+import settings from "./settings.svg";
+import chart from "./chart.svg";
+import quotes from "./quotes.svg";
+import avatarAiysha from "./avatarAiysha.jpg";
+import avatarKady from "./avatarKady.jpg";
+import avatarArthur from "./avatarArthur.jpg";
+import patternHeroDown from "./patternHeroDown.svg";
+import patternHeroLeft from "./patternHeroLeft.svg";
+import patternMenuRight from "./patternMenuRight.svg";
+import patternFeaturesRight from "./patternFeaturesRight.svg";
+import patternTestimonialTop from "./patternTestimonialTop.svg";
+import patternTestimonialBottom from "./patternTestimonialBottom.svg";
+import patternCTA from "./patternCTA.svg";
+
+export {
+  logo,
+  hamburger,
+  open,
+  close,
+  person,
+  settings,
+  chart,
+  avatarAiysha,
+  avatarKady,
+  avatarArthur,
+  quotes,
+  patternHeroDown,
+  patternHeroLeft,
+  patternMenuRight,
+  patternFeaturesRight,
+  patternTestimonialTop,
+  patternTestimonialBottom,
+  patternCTA,
+};
